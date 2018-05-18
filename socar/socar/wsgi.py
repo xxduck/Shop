c8740c71bfe93385a17461817e1c971e2bda229e
xiaofang-git <fang.1995@outlook.com>
2018-05-18 17:17:52 +0800

    优化项目结构

--- socar/socar/wsgi.py
+++ socar/socar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Shop project.
+WSGI config for socar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Shop.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socar.settings")
 
 application = get_wsgi_application()
