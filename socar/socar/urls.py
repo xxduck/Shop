e35c2c7c6a2fc490f7c965aadd49d16e6d510732
xiaofang-git <fang.1995@outlook.com>
2018-05-18 17:31:28 +0800

    添加shop app

--- socar/socar/urls.py
+++ socar/socar/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shop.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
 ]
