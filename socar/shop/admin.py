2e427c34cfafacf8a99c30048183760a6254e2d4
xiaofang-git <fang.1995@outlook.com>
2018-05-21 14:40:46 +0800

    修改后台管理模型

--- socar/shop/admin.py
+++ socar/shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Commodity
 
 # Register your models here.
+
+
+class CommodityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'id',
+                'name'
+            )
+        })
+    )
+
+
+admin.register(Commodity, CommodityAdmin)
--- socar/shop/models.py
+++ socar/shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Commodity(models.Model):
+    #  商品
+    STATUS = (
+        (1, "在售"),
+        (2, "缺货"),
+        (3, "下架"),
+    )
+    name = models.CharField(verbose_name='商品名称', max_length=255)
+    price = models.FloatField(verbose_name="商品价格")
+    status = models.IntegerField(verbose_name="商品状态",choices=STATUS)
